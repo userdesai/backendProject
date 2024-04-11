c8bc33429c5c062ff5ada4bc504534796b458437
Aniket <120aniket4033@sjcem.edu.in>
2024-04-11 19:09:20 +0530

    small changes

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -3,6 +3,6 @@ class ApiResponse{
         this.statuscode=statuscode
         this.data=data
         this.message=message
-        this.success=statuscode<400
+        this.success=statuscode
     }
 }
