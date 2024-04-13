06f107c0b32dae5a9354ea8e24fb7e4b260f5fa0
Aniket <120aniket4033@sjcem.edu.in>
2024-04-13 11:27:09 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
-const app=express
+const app=express()
 //middleware or configuration me app.use() karte hain
 app.use(cors({
     origin:process.env.CORS_ORIGIN,
@@ -13,5 +13,15 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+
+//routes import
+import userRouter from './routes/user.routes.js' 
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
+
+//http://localhost:8000/users/login
 
 export {app}
